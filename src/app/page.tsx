7edab6043ddb0b9fd57f8c6899e9fe7d34a37c56
Vaggelis Karavasileiadis <e.karavasileiadis@gmail.com>
2023-08-26 21:30:38 +0300

    Tailwind prettier plugin setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <Button>Button</Button>
-    </div>
-  );
+  return <main>Test</main>;
 }
