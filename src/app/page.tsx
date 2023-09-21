96ddb4d8ff83402d147a616053c4e4831975def4
VaggelisKa <e.karavasileiadis@gmail.com>
2023-09-21 22:56:29 +0200

    Change scripts and test tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <main>
-      Test <Button>test</Button>
+      <h1 className="text-2xl font-bold">Welcome, user</h1>
     </main>
   );
 }
