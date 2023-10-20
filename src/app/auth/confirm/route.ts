048421bc742499543a3af7b8b5da7291e5d4e8d8
VaggelisKa <e.karavasileiadis@gmail.com>
2023-10-20 21:11:46 +0200

    Fix type

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -1,11 +1,13 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { EmailOtpType } from "@supabase/supabase-js";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
+
   const token_hash = searchParams.get("token_hash");
-  const type = searchParams.get("type") as any;
+  const type = searchParams.get("type") as EmailOtpType;
   const next = searchParams.get("next") ?? "/";
 
   if (token_hash && type) {
