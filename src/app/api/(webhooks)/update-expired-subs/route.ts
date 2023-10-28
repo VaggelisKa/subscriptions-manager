fd8a619397a5b6fb4682630177adf44c002eb5de
VaggelisKa <e.karavasileiadis@gmail.com>
2023-10-28 11:28:18 +0200

    Create vercel cron and finalize route handler

--- src/app/api/(webhooks)/update-expired-subs/route.ts
+++ src/app/api/(webhooks)/update-expired-subs/route.ts
@@ -1,4 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
+import { add } from "date-fns";
 
 export async function PUT() {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
@@ -17,13 +18,37 @@ export async function PUT() {
   });
   const { error, data } = await supabase
     .from("subscriptions")
-    .select("*")
+    .select("billed_at, id, interval")
     .lte("billed_at", new Date().toISOString());
-
-  console.log(data);
 
   if (error) {
     return Response.json({ error: error.message }, { status: 500 });
+  }
+
+  if (!data?.length) {
+    return Response.json(
+      { message: "No subscriptions to update" },
+      { status: 200 },
+    );
+  }
+
+  for (let row of data) {
+    const { billed_at, id, interval } = row;
+
+    if (!billed_at) {
+      continue;
+    }
+
+    const newDate = add(new Date(billed_at), {
+      weeks: interval === "week" ? 1 : 0,
+      months: interval === "month" ? 1 : 0,
+      years: interval === "year" ? 1 : 0,
+    }).toISOString();
+
+    await supabase
+      .from("subscriptions")
+      .update({ billed_at: newDate })
+      .eq("id", id);
   }
 
   return Response.json({}, { status: 201 });
